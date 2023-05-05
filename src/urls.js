bca023de84b3cfe3ec3c8741556a61c391361fac
Pieter den Toom <pieter@experiencerepublic.nl>
2023-05-05 13:44:33 +0200

    All functionality added for adult route

--- src/urls.js
+++ src/urls.js
@@ -3,4 +3,9 @@ export const urls = {
     image_2: new URL('./assets/2 groen.png', import.meta.url),
     image_3: new URL('./assets/3 groen.png', import.meta.url),
     image_4: new URL('./assets/4 groen.png', import.meta.url),
+    error: new URL('./assets/audio/error.mp3', import.meta.url),
+    heartbeat: new URL('./assets/audio/heartbeat.mp3', import.meta.url),
+    success: new URL('./assets/audio/success.mp3', import.meta.url),
 }
+
+
